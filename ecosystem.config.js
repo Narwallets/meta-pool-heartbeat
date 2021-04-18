a2585f42b1a2c7c0a7f56cb07f2d5982dddb0192
luciotato <luciotato@gmail.com>
2021-04-18 17:35:36 -0300

    fix v1 script name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: 'v1-meta-pool-heartbeat',
     cwd: 'dist-v1',
-    script: 'main-v1.js',
+    script: 'main.js',
     restart_delay: 1000,
     watch: 'main.js',
     out_file: 'main.log',
